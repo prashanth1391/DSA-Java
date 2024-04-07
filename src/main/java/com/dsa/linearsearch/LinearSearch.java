2209e1482f7219d9e4433efb4afabb7c8208660d
Prashanth <prashanthkumarcse@gmail.com>
2024-04-07 21:03:04 +0530

    Code formatted in linear search

--- src/main/java/com/dsa/linearsearch/LinearSearch.java
+++ src/main/java/com/dsa/linearsearch/LinearSearch.java
@@ -2,10 +2,10 @@ package com.dsa.linearsearch;
 
 public class LinearSearch {
 
-    static int doLinearSearch(int[] arr, int target){
-        if(arr.length ==- 0) return -1;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==target) return i;
+    static int doLinearSearch(int[] arr, int target) {
+        if (arr.length == 0) return -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) return i;
         }
         return -1;
     }
